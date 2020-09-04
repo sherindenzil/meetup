40420ff266982a52fcf989842a653b21a4e1492f
Sherin-Denzil <nysherin.denzil@gmail.com>
2020-09-03 23:29:13 -0400

    erroralert added

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
   state = {
     numberOfEvents: 32,
+    errorText: "",
   };
 
   handleInputChanged = (event) => {
     const value = event.target.value;
+    if (value < 1) this.setState({ errorText: "Please enter a valid number" });
+    else this.setState({ errorText: "" });
     this.setState({ numberOfEvents: value });
     this.props.updateEvents(null, null, value);
   };
@@ -14,6 +18,7 @@ class NumberOfEvents extends Component {
   render() {
     return (
       <div className="numberOfEvents">
+        <ErrorAlert text={this.state.errorText} />
         <label>Number of Events: </label>
         <input
           type="text"
